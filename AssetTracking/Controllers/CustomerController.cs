2477b2f620b8dc0a8a5451fedb18ff83cc7492bd
Shibly Mohammad <sun8496@gmail.com>
2017-05-21 01:20:01 +0600

    first commit

---
+++ AssetTracking/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetTracking.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+        public ActionResult Insert()
+        {
+            return View();
+        }
+    }
+}
